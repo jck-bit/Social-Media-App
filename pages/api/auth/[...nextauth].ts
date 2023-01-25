1bae064e46f287dddbd67378d66d286e34cc2edd
jck-bit <jckkinyanjui@gmail.com>
2023-01-25 19:38:26 +0300

    Added next-auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,1 +1,19 @@
-import NextAuth from "next-auth/next";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import  CredentialsProvider  from "next-auth/providers/credentials";
+
+const authOptions:NextAuthOptions ={
+    session:{
+        strategy: 'jwt'
+    },
+    providers:[
+        CredentialsProvider({
+            type:'credentials',
+            credentials:{},
+            authorize(credentials, req){
+                const {} = credentials
+            }
+        })
+    ]
+}
+
+export default NextAuth(authOptions);
