14a607b7ec31fb48a8064491760b4884bf3beaca
jck-bit <jckkinyanjui@gmail.com>
2023-01-25 23:04:08 +0300

    Auth is now functional with credentials

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,19 +1,37 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
-import  CredentialsProvider  from "next-auth/providers/credentials";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connect } from "@/utils/connection";
+import bcrypt from "bcrypt";
+import { NextApiRequest, NextApiResponse } from "next";
 
-const authOptions:NextAuthOptions ={
-    session:{
-        strategy: 'jwt'
-    },
-    providers:[
-        CredentialsProvider({
-            type:'credentials',
-            credentials:{},
-            authorize(credentials, req){
-                const {} = credentials
-            }
-        })
-    ]
-}
+const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    CredentialsProvider({
+      type: "credentials",
+      credentials: {},
+      authorize: async(credentials:any, req:NextApiRequest, res:NextApiResponse) => {
+        const { email, password } = credentials as {
+          email: string;
+          password: string;
+        };
+        const { User } = await connect();
+
+        const user = await User.findOne({ email }).lean().exec()
+        if (!user)  return {message:"user does not exist"}
+        
+        const isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch) return {msg:"Invalid credentials"}
+        
+        return {user}
+      }
+    }),
+  ],
+  pages: {
+    signIn: "/auth/signin",
+  },
+};
 
 export default NextAuth(authOptions);
