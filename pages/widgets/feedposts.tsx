a0c0c503b5b4738891c4166296cc00f94d79117b
jck-bit <jckkinyanjui@gmail.com>
2023-01-29 18:54:00 +0300

    used client-side fetching(getServerSideProps)-not working

--- pages/widgets/feedposts.tsx
+++ pages/widgets/feedposts.tsx
@@ -1,21 +1,44 @@
-import React from 'react'
-import { Post } from '@/utils/types'
+import React, { useEffect, useState } from 'react';
+import { Post } from '@/utils/types';
 
 interface Props {
-    data: Post[]
+  posts: Post[];
 }
 
-const Posts = () => {
+const Posts: React.FC<Props> = ({ posts }) => {
+  const [data, setData] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('http://localhost:3000/api/posts');
+      const jsonData = await response.json();
+
+      setData(jsonData);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <main className='main_container'>
-       <div className="the_post">
-        <textarea placeholder='say something...'></textarea>
-       </div>
-       <div className="post_button">
-      <input type="submit" value="Post" className='create_post_submit' />
-       </div>
+    <main className="main_container">
+      <div className="input_container">
+        <div className="the_post">
+          <textarea placeholder="say something..." />
+        </div>
+        <div className="post_button">
+          <input type="submit" value="Post" className="create_post_submit" />
+        </div>
+      </div>
+      <div className="main_posts">
+        {data.map((post: Post) => (
+          <>
+            <h1>{post.content}</h1>
+            <h2>Hello</h2>
+          </>
+        ))}
+      </div>
     </main>
-    )
-}
+  );
+};
 
-export default Posts
+export default Posts;
