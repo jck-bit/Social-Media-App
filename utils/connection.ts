93c0413c4197c3c2fbe560a29d620aed19bcda3e
jck-bit <jckkinyanjui@gmail.com>
2023-01-30 11:55:52 +0300

    Gave each user a default image whenever they signup

--- utils/connection.ts
+++ utils/connection.ts
@@ -19,7 +19,7 @@ export const connect = async () => {
       username: { type: String, required: true },
       email: { type: String, required: true, unique: true },
       password: { type: String, required: true },
-      image: { type: String, default: "https://www.shutterstock.com/image-photo/building-background-hd-images-without-600w-1907638465.jpg" },
+      image: { type: String, default: "" },
     });
 
     const PostSchema = new mongoose.Schema({
