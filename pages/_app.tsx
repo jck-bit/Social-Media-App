2776661515dc68df1fd03f9dbf49971ae9087325
jck-bit <jckkinyanjui@gmail.com>
2023-02-03 16:28:36 +0300

    Edited the app.tsx filr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,14 +7,9 @@ import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import { Session } from 'next-auth'
 
-function MyApp({
-  Component,
-  pageProps,
-}: AppProps<{
-  session: Session;
-}>) {
-  
-  return (
+
+function MyApp({Component, pageProps}: AppProps<{session: Session}>) {
+    return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
     </SessionProvider>
