5321e4cf40360a1bdfa87c1c130c0501be0ecc34
jck-bit <jckkinyanjui@gmail.com>
2023-02-04 14:14:13 +0300

    styled the login page

---
+++ components/Load.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Load = () => {
+  return (
+    <div className="center">
+  <div className="wave"></div>
+  <div className="wave"></div>
+  <div className="wave"></div>
+  <div className="wave"></div>
+  <div className="wave"></div>
+  <div className="wave"></div>
+  <div className="wave"></div>
+  <div className="wave"></div>
+  <div className="wave"></div>
+  <div className="wave"></div>
+</div>
+  )
+}
+
+export default Load
--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -2,6 +2,8 @@ import { signIn } from "next-auth/react";
 import { FormEventHandler, useEffect, useState } from "react";
 import { useRouter } from 'next/router'
 import { useSession } from "next-auth/react";
+import Loader from "../loader";
+import Load from "@/components/Load";
 
 const SignIn = () => {
   const router = useRouter()
@@ -14,54 +16,59 @@ const SignIn = () => {
   })
 
   const [userInfo, setUserInfo] = useState({ email: "", password: "" });
+  const [loading, setLoading] = useState(false);
   const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
+    setLoading(true);
 
     const res = await signIn("credentials", {
       email: userInfo.email,
       password: userInfo.password,
       redirect: false,
     });
-
     console.log(res);
+    setLoading(false)
   };
   return (
     <div className="sign-in-form">
-      Welcome
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-        <label>Email</label>
-        <input 
-          value={userInfo.email}
-          onChange={({ target }) =>
-            setUserInfo({ ...userInfo, email: target.value })
-          }
-          type="email"
-          placeholder="Email"
-          className="form-control"
-          name="email"
-          required
-        />
-        </div>
-        <div className="form-group">
-        <label>Password</label>
-        <input
-          value={userInfo.password}
-          onChange={({ target }) =>
-            setUserInfo({ ...userInfo, password: target.value })
-          }
-          type="password"
-          placeholder="********"
-          className="form-control"
-          name="password"
-          required
-        />
-        </div>
-        <div className="signup-button">
-        <a type="button" onClick={() => router.push('/auth/signup')}>Dont have an account?  signup</a>
-        <input type="submit" value="signin" className="btn btn-primary"/>
-        </div>
-      </form>
+      {loading ? (
+        <Load/>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+          <label>Email</label>
+          <input 
+            value={userInfo.email}
+            onChange={({ target }) =>
+              setUserInfo({ ...userInfo, email: target.value })
+            }
+            type="email"
+            placeholder="Email"
+            className="form-control"
+            name="email"
+            required
+          />
+          </div>
+          <div className="form-group">
+          <label>Password</label>
+          <input
+            value={userInfo.password}
+            onChange={({ target }) =>
+              setUserInfo({ ...userInfo, password: target.value })
+            }
+            type="password"
+            placeholder="********"
+            className="form-control"
+            name="password"
+            required
+          />
+          </div>
+          <div className="signup-button">
+            <a type="button"  className="sign_up_tag" onClick={() => router.push('/auth/signup')}>Dont have an account?  signup</a>
+            <input type="submit" value="signin" className="btn btn-primary"/>
+          </div>
+        </form>
+      )}
     </div>
   );
 };
